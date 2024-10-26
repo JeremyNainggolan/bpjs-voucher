6417fd77098df615398f726df6d9844a664c0925
Jeremy Mart Valentino Nainggolan <jeremy.nainggolann@gmail.com>
2024-10-27 00:13:57 +0700

    Update login.blade.php

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -16,13 +16,14 @@
                                 <div class="my-4">
                                     <label for="identity" class="fw-bolder mb-1">Pilih Jenis Identitas</label>
                                     <select class="form-select fw-light" aria-label="Default select example"
-                                            name="identity">
+                                            name="identity" id="identity" onchange="updatePlaceholder()">
                                         <option value="nik_select" selected>Nomor Induk Kependudukan (NIK)</option>
                                         <option value="jkn_select">Nomor Kartu JKN</option>
                                     </select>
                                 </div>
                                 <div class="my-4">
-                                    <label for="nik" class="fw-bolder mb-1">Nomor Induk Kependudukan (NIK)</label>
+                                    <label for="nik" class="fw-bolder mb-1" id="label_nik">Nomor Induk Kependudukan
+                                        (NIK)</label>
                                     <input id="nik" name="nik" type="text" class="form-control" required
                                            placeholder="16 Digit Nomor Induk Kependudukan">
                                 </div>
@@ -44,7 +45,9 @@
                                     </button>
                                 </div>
                                 <div class="mt-1 text-center mt-2">
-                                    <p class="fw-bolder card-text"><a href="{{ url('lupa-kata-sandi') }}" class="text-decoration-none">Lupa Kata Sandi</a></p>
+                                    <p class="fw-bolder card-text"><a href="{{ url('lupa-kata-sandi') }}"
+                                                                      class="text-decoration-none">Lupa Kata Sandi</a>
+                                    </p>
                                 </div>
                             </form>
                         </div>
@@ -95,5 +98,21 @@
             }
         }
 
+        function updatePlaceholder() {
+            var identitySelect = document.getElementById("identity");
+            var selectedValue = identitySelect.value;
+            var nikInput = document.getElementById("nik");
+            var labelNik = document.getElementById("label_nik");
+
+            if (selectedValue === "nik_select") {
+                nikInput.placeholder = "16 Digit Nomor Induk Kependudukan";
+                labelNik.textContent = "Nomor Induk Kependudukan (NIK)";
+            } else if (selectedValue === "jkn_select") {
+                nikInput.placeholder = "Nomor Kartu JKN";
+                labelNik.textContent = "Nomor Kartu JKN";
+            }
+        }
+
     </script>
+
 @endsection
